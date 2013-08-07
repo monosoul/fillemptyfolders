72069c79d5afb898fd8b032c6df991754155c57c
monosoul <kloz.klaud@gmail.com>
2013-08-07 22:07:05 +0600

    Made output more useful

--- fillemptyfolders.sh
+++ fillemptyfolders.sh
@@ -5,10 +5,10 @@ function crgitignore(){
 	if [ -n "$isempty" ] ; then
 		for dir in $(ls -A1p ${1} | grep "/" | sed -e "s/\///g")
 		do
-			echo ${1}/$dir
 			crgitignore ${1}/$dir
 		done
 	else
+		echo ${1}
 		touch ${1}/.gitignore
 	fi
 }
